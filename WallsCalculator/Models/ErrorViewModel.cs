3568fc59a6afd63fb33bb5f0853a145bd5efc3ef
Фаттахов Нияз Ильдусович <niyaz.fattakhov@akbarsdigital.ru>
2023-02-11 12:11:23 +0300

    Добавьте файлы проекта.

---
+++ WallsCalculator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WallsCalculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
