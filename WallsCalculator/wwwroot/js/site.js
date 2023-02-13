b02f3c7c93527c1653f0fa31760fd96e52c590d7
Babahan Kurbanov <baha.k@ip-ltd.ru>
2023-02-13 23:24:42 +0300

    fix

--- WallsCalculator/wwwroot/js/site.js
+++ WallsCalculator/wwwroot/js/site.js
@@ -1,4 +1,4 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
+// console.log('today we'll talk about dot net framework')
 // Write your JavaScript code.
